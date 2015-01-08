d6166025515fce5f2c16d75adf1d5c103cd29937
Amit Shekhar <ashekhar@codeaurora.org>
2015-01-08 15:11:34 -0800

    ASoC: msm: qdsp6v2: Fix asm bitwidth for next track in gapless mode
    asm is opened at fixed bitwidth 16 for next track in gapless mode.
    This is causing noise when a 24 bit clip is played in single repeat
    gapless mode.
    By opening asm based on input stream bitwidth, asm is configured
    at appropriate bitwidth.
    
    Change-Id: Icc815d0a812fa17ae981cdeaf8e56142623deb79
    CRs-Fixed: 778865
    Signed-off-by: Amit Shekhar <ashekhar@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -686,8 +686,13 @@ static int msm_compr_configure_dsp(struct snd_compr_stream *cstream)
 		.step = SOFT_VOLUME_STEP,
 		.rampingcurve = SOFT_VOLUME_CURVE_LINEAR,
 	};
+	if (prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S24_LE)
+		bits_per_sample = 24;
+	else if (prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S32_LE)
+		bits_per_sample = 32;
 
-	pr_debug("%s: stream_id %d\n", __func__, ac->stream_id);
+	pr_debug("%s: stream_id %d bits_per_sample %d\n",
+			__func__, ac->stream_id, bits_per_sample);
 #ifdef VENDOR_EDIT
      //guoguangyi@mutimedia.2016.04.23,
     //use 24bits to get rid of 16bits innate noise
@@ -1530,9 +1535,17 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 			}
 			break;
 		}
-		pr_debug("%s: open_write stream_id %d", __func__, stream_id);
+
+		if (prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S24_LE)
+			bits_per_sample = 24;
+		else if (prtd->codec_param.codec.format ==
+			 SNDRV_PCM_FORMAT_S32_LE)
+			bits_per_sample = 32;
+
+		pr_debug("%s: open_write stream_id %d bits_per_sample %d",
+				__func__, stream_id, bits_per_sample);
 		rc = q6asm_stream_open_write_v2(prtd->audio_client,
-				prtd->codec, 16,
+				prtd->codec, bits_per_sample,
 				stream_id,
 				prtd->gapless_state.use_dsp_gapless_mode);
 		if (rc < 0) {
