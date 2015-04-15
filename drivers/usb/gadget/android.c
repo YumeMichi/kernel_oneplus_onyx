2fb0d7b60ec2e11ce2cc1d8954570a6306454497
Kitty Ho <kittyh@codeaurora.org>
2015-04-15 15:27:02 -0700

    usb: android: Add HID function driver to android gadget driver
    The current Android Gadget Driver does not support HID as one of the
    functions.
    
    The HID function driver supports a single device with one boot
    interface that does not specify any protocol.
    
    Add HID report descriptor that only states the basic report of
    a HID driver and does not support any mouse or keyboard functionalities.
    
    Change-Id: I8c8d73eb63136fd4d49b27e9d0bcf60a15716bd5
    Signed-off-by: Kitty Ho <kittyh@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -88,6 +88,7 @@
 #include "f_qc_rndis.c"
 #include "u_ether.c"
 #include "u_qc_ether.c"
+#include "f_hid.c"
 #ifdef CONFIG_TARGET_CORE
 #include "f_tcm.c"
 #endif
@@ -1346,6 +1347,29 @@ static struct android_usb_function audio_function = {
 };
 #endif
 
+static int hid_function_init(struct android_usb_function *f,
+				 struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f,
+					struct usb_configuration *c)
+{
+	return hidg_bind_config(c, NULL, 0);
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
 
 /* DIAG */
 static char diag_clients[32];	    /*enabled DIAG clients- "diag[,diag_mdm]" */
@@ -2338,6 +2362,7 @@ static struct android_usb_function *supported_functions[] = {
 	&ncm_function,
 	&mass_storage_function,
 	&accessory_function,
+	&hid_function,
 #ifdef CONFIG_SND_PCM
 	&audio_source_function,
 #endif
