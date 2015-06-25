f7de2fbcb2eeb38412bb058e11ee73251818940a
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2015-06-25 15:00:27 -0700

    zram: cosmetic zram_bvec_write() cleanup
    `bool locked' local variable tells us if we should perform
    zcomp_strm_release() or not (jumped to `out' label before
    zcomp_strm_find() occurred), which is equivalent to `zstrm' being or not
    being NULL.  remove `locked' and check `zstrm' instead.
    
    Change-Id: I92649164ade52c75a295034a7737b4ef9333a80b
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -647,9 +647,8 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	struct page *page;
 	unsigned char *user_mem, *cmem, *src, *uncmem = NULL;
 	struct zram_meta *meta = zram->meta;
-	struct zcomp_strm *zstrm;
+	struct zcomp_strm *zstrm = NULL;
 	static unsigned long zram_rs_time;
-	bool locked = false;
 	unsigned long alloced_pages;
 
 	page = bvec->bv_page;
@@ -669,7 +668,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	}
 
 	zstrm = zcomp_strm_find(zram->comp);
-	locked = true;
 	user_mem = kmap_atomic(page);
 
 	if (is_partial_io(bvec)) {
@@ -743,7 +741,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	}
 
 	zcomp_strm_release(zram->comp, zstrm);
-	locked = false;
+	zstrm = NULL;
 	zs_unmap_object(meta->mem_pool, handle);
 
 	/*
@@ -761,7 +759,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	atomic64_add(clen, &zram->stats.compr_data_size);
 	atomic64_inc(&zram->stats.pages_stored);
 out:
-	if (locked)
+	if (zstrm)
 		zcomp_strm_release(zram->comp, zstrm);
 	if (is_partial_io(bvec))
 		kfree(uncmem);
