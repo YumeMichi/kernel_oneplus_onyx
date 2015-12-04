721795ac56fa79820a238004a421e9afc9b27c81
Sultanxda <sultanxda@gmail.com>
2015-12-03 18:58:42 -0800

    msm: mdss: Treat doze as normal unblank
    In order for the HWC hack for ambient display to work without breaking
    anything, a separate blank mode (FB_BLANK_VSYNC_SUSPEND) must be used to
    safely filter out bogus doze events (currently, the display HAL uses
    FB_BLANK_UNBLANK for ambient display). True doze support is not currently
    present in our MDSS, so just treat doze as a normal unblank as it has
    always been treated up until now.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1130,7 +1130,6 @@ static int mdss_fb_blank_sub(int blank_mode, struct fb_info *info,
 		break;
 
 	case FB_BLANK_VSYNC_SUSPEND:
-		req_power_state = MDSS_PANEL_POWER_DOZE;
 		pr_debug("Doze power mode requested\n");
 
 		/*
@@ -1145,10 +1144,7 @@ static int mdss_fb_blank_sub(int blank_mode, struct fb_info *info,
 				ret = mdss_fb_unblank_sub(mfd);
 			}
 		}
-
-		/* Enter doze mode only if unblank succeeded */
-		if (ret)
-			break;
+		break;
 	case FB_BLANK_HSYNC_SUSPEND:
 	case FB_BLANK_NORMAL:
 	case FB_BLANK_POWERDOWN:
@@ -1211,10 +1207,9 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 
 	mdss_fb_pan_idle(mfd);
 	if (mfd->op_enable == 0) {
-		if (blank_mode == FB_BLANK_UNBLANK)
+		if (blank_mode == FB_BLANK_UNBLANK ||
+			blank_mode == FB_BLANK_VSYNC_SUSPEND)
 			mfd->suspend.panel_power_state = MDSS_PANEL_POWER_ON;
-		else if (blank_mode == FB_BLANK_VSYNC_SUSPEND)
-			mfd->suspend.panel_power_state = MDSS_PANEL_POWER_DOZE;
 		else
 			mfd->suspend.panel_power_state = MDSS_PANEL_POWER_OFF;
 		return 0;
