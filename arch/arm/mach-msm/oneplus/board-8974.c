14c83f81067b5bf137766311036f2217b9e9d585
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-04 04:11:33 +0000

    msm: thermal: Add my simplified thermal driver
    Stock thermal-engine goes crazy too soon and too often and offers
    no way for the userspace to tweak its parameters
    
    Change-Id: Ie1253fae004fa215ed5003167e9aa55e7574c503
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/oneplus/board-8974.c
+++ arch/arm/mach-msm/oneplus/board-8974.c
@@ -134,7 +134,6 @@ void __init msm8974_add_drivers(void)
 	else
 		msm_clock_init(&msm8974_clock_init_data);
 	tsens_tm_init_driver();
-	msm_thermal_device_init();
 	platform_device_register(&ram_console_device);
 }
 
