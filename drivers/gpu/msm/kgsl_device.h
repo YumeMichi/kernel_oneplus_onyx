5f0d637a2b6f7d1b4ab46e1df5fc42d18cf973a2
Jordan Crouse <jcrouse@codeaurora.org>
2016-05-31 17:24:30 +0000

    msm: kgsl: Remove event worker
    Scheduling issues were occurring with the GPU event worker after
    b7be807 (msm: kgsl: Unbind the kgsl-event workqueue) was merged.
    In certain conditions, it seems that the kgsl-event workqueue
    was conflicting with the KGSL worker and slowing it down.
    
    It turns out that everywhere we schedule the event worker
    and the dispatcher worker at the same time.  Since the worker
    is singlethread, the event worker and the dispatcher run
    synchronously anyway, so it makes sense to run the event processor
    from within the dispatcher and save the extra schedule.
    
    Change-Id: Ic0dedbad67eb04d41afb6add4477f146dfff9784
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -356,7 +356,6 @@ struct kgsl_device {
 	int pm_dump_enable;
 	struct kgsl_pwrscale pwrscale;
 	struct kobject pwrscale_kobj;
-	struct work_struct event_work;
 
 	/* Postmortem Control switches */
 	int pm_regs_enabled;
@@ -376,8 +375,6 @@ struct kgsl_device {
 	.cmdbatch_gate = COMPLETION_INITIALIZER((_dev).cmdbatch_gate),\
 	.idle_check_ws = __WORK_INITIALIZER((_dev).idle_check_ws,\
 			kgsl_idle_check),\
-	.event_work  = __WORK_INITIALIZER((_dev).event_work,\
-			kgsl_process_events),\
 	.context_idr = IDR_INIT((_dev).context_idr),\
 	.wait_queue = __WAIT_QUEUE_HEAD_INITIALIZER((_dev).wait_queue),\
 	.active_cnt_wq = __WAIT_QUEUE_HEAD_INITIALIZER((_dev).active_cnt_wq),\
@@ -609,7 +606,7 @@ int kgsl_add_event(struct kgsl_device *device, struct kgsl_event_group *group,
 void kgsl_process_event_group(struct kgsl_device *device,
 	struct kgsl_event_group *group);
 
-void kgsl_process_events(struct work_struct *work);
+void kgsl_process_event_groups(struct kgsl_device *device);
 
 static inline struct kgsl_device_platform_data *
 kgsl_device_get_drvdata(struct kgsl_device *dev)
