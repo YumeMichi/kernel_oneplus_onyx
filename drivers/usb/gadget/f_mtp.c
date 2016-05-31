b3d55e4d7ee0ce89fc97af91111ac3a3bef6ba63
Sultanxda <sultanxda@gmail.com>
2016-05-30 20:43:37 -0700

    USB: gadget: f_mtp: Check endpoints in MTP r/w nodes
    These can be NULL, so check them.
    
    Change-Id: I8ba4f53623108ef4a9222b02c2fb7defe83d4d1e

--- drivers/usb/gadget/f_mtp.c
+++ drivers/usb/gadget/f_mtp.c
@@ -701,6 +701,9 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 	DBG(cdev, "mtp_read(%d)\n", count);
 	#endif
 
+	if (!dev->ep_out)
+		return -EINVAL;
+
 	len = ALIGN(count, dev->ep_out->maxpacket);
 
 	if (len > mtp_rx_req_len)
@@ -800,6 +803,9 @@ static ssize_t mtp_write(struct file *fp, const char __user *buf,
 	#else
 	DBG(cdev, "mtp_write(%d)\n", count);
 	#endif
+
+	if (!dev->ep_in)
+		return -EINVAL;
 
 	spin_lock_irq(&dev->lock);
 	if (dev->state == STATE_CANCELED) {
