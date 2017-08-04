8622c105554ae78fbe55c8e977229a3ab9e3b561
Tom Marshall <tdm.code@gmail.com>
2017-08-04 21:56:24 +0000

    ASoC: msm: qdsp6v2: Fix build after mismerge
    Change-Id: Ib8468835f3bc8882cb59acd22de9e5bb256f53b1

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -1546,7 +1546,7 @@ static int msm_routing_ext_ec_put(struct snd_kcontrol *kcontrol,
 		 __func__, msm_route_ext_ec_ref,
 		 ucontrol->value.integer.value[0]);
 
-	if (mux >= e->items) {
+	if (mux >= e->max) {
 		pr_err("%s: Invalid mux value %d\n", __func__, mux);
 		return -EINVAL;
 	}
@@ -4298,7 +4298,7 @@ static int msm_pcm_routing_hw_params(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 
-	if (mux >= e->items) {
+	if (mux >= e->max) {
 		pr_err("%s: Invalid mux value %d\n", __func__, mux);
 		return -EINVAL;
 	}
