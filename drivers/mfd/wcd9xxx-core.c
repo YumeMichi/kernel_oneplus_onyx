c9c7c9cddd99d0f2dfb50e6a8fc87512b8cc6eff
Karthikeyan Mani <kmani@codeaurora.org>
2017-10-02 12:38:10 -0700

    ASoC: wcd9xxx: restrict debugfs permission
    Remove read permission for debugfs reg dump node
    for group and users to not allow reading of wcd9xxx
    registers.
    
    CRs-fixed: 2113240
    Bug: 62464339
    Change-Id: I73a22e140446828e694fdc95fde7ac4e051c9548
    Signed-off-by: Karthikeyan Mani <kmani@codeaurora.org>
    CVE-2017-15850
    [haggertk: Backport to 3.4/msm8974]
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    (cherry picked from commit bca5f12bdc5d70dcd38f515aa0197f0de5c62536)

--- drivers/mfd/wcd9xxx-core.c
+++ drivers/mfd/wcd9xxx-core.c
@@ -1723,11 +1723,11 @@ if(!get_smartpa_project())
 		("wcd9310_slimbus_interface_device", 0);
 	if (!IS_ERR(debugfs_wcd9xxx_dent)) {
 		debugfs_peek = debugfs_create_file("peek",
-		S_IFREG | S_IRUGO, debugfs_wcd9xxx_dent,
+		S_IFREG | S_IRUSR, debugfs_wcd9xxx_dent,
 		(void *) "peek", &codec_debug_ops);
 
 		debugfs_poke = debugfs_create_file("poke",
-		S_IFREG | S_IRUGO, debugfs_wcd9xxx_dent,
+		S_IFREG | S_IRUSR, debugfs_wcd9xxx_dent,
 		(void *) "poke", &codec_debug_ops);
 	}
 #endif
