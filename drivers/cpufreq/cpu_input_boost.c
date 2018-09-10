kerneltoast/android_kernel_oneplus_msm8996

cpu_input_boost: Trigger max boost when button is pressed during suspend
In order to reduce device wake-up latency, boost as soon as a button press
is received, as it can take a while for the framebuffer notifier to kick in
after that.

Signed-off-by: Sultanxda <sultanxda@gmail.com>

drivers/cpufreq/cpu_input_boost.c
@@ -60,6 +60,7 @@ struct boost_policy {
 	struct ib_config ib;
 	struct workqueue_struct *wq;
 	uint8_t enabled;
+	bool device_awake;
 };
 
 static struct boost_policy *boost_policy_g;
@@ -203,8 +204,16 @@ static int fb_unblank_boost(struct notifier_block *nb,
 	int *blank = evdata->data;
 
 	/* Only boost for unblank (i.e. when device is woken) */
-	if (val != FB_EARLY_EVENT_BLANK || *blank != FB_BLANK_UNBLANK)
+	if (val != FB_EARLY_EVENT_BLANK)
 		return NOTIFY_OK;
+
+	/* Keep track of suspend state */
+	if (*blank == FB_BLANK_UNBLANK) {
+		b->device_awake = 1;
+	} else {
+		b->device_awake = 0;
+		return NOTIFY_OK;
+	}
 
 	if (!is_driver_enabled(b))
 		return NOTIFY_OK;
@@ -228,6 +237,12 @@ static void cpu_ib_input_event(struct input_handle *handle, unsigned int type,
 {
 	struct boost_policy *b = handle->handler->private;
 	bool do_boost;
+
+	/* Anticipate device wake-up and boost early */
+	if (!b->device_awake) {
+		queue_work(b->wq, &b->fb.boost_work);
+		return;
+	}
 
 	spin_lock(&b->lock);
 	do_boost = b->enabled && !b->fb.state && !b->ib.running;

YumeMichi/kernel_oneplus_onyx

Change-Id: Ia39bd9cf54bb060a40a313fe43cd4fcb7e69c093

drivers/cpufreq/cpu_input_boost.c
@@ -58,6 +58,7 @@ struct boost_policy {
 	struct ib_config ib;
 	struct workqueue_struct *wq;
 	uint8_t enabled;
+	bool device_awake;
 };
 
 static struct boost_policy *boost_policy_g;
@@ -215,8 +216,16 @@ static int fb_unblank_boost(struct notifier_block *nb,
 	int *blank = evdata->data;
 
 	/* Only boost for unblank (i.e. when device is woken) */
-	if (val != FB_EVENT_BLANK || *blank != FB_BLANK_UNBLANK)
+	if (val != FB_EVENT_BLANK)
 		return NOTIFY_OK;
+
+	/* Keep track of suspend state */
+	if (*blank == FB_BLANK_UNBLANK) {
+		b->device_awake = 1;
+	} else {
+		b->device_awake = 0;
+		return NOTIFY_OK;
+	}
 
 	if (!is_driver_enabled(b))
 		return NOTIFY_OK;
@@ -241,6 +250,12 @@ static void cpu_ib_input_event(struct input_handle *handle, unsigned int type,
 	struct boost_policy *b = handle->handler->private;
 	enum boost_status ib_status;
 	bool do_boost;
+
+	/* Anticipate device wake-up and boost early */
+	if (!b->device_awake) {
+		queue_work(b->wq, &b->fb.boost_work);
+		return;
+	}
 
 	spin_lock(&b->lock);
 	ib_status = b->ib.running;