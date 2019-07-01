7ebff01c9d982359bb10fc406f18da6de9ed446c
Al Viro <viro@zeniv.linux.org.uk>
2019-07-01 23:47:19 +0300

    cache the value of file_inode() in struct file
    Note that this thing does *not* contribute to inode refcount;
    it's pinned down by dentry.
    
    Change-Id: I7c27b7c93d79455b51db2891a7e0b2ace0c9294f
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1937,10 +1937,11 @@ static inline bool f2fs_cp_error(struct f2fs_sb_info *sbi)
 	return is_set_ckpt_flags(sbi, CP_ERROR_FLAG);
 }
 
+/* Implementation of file_inode added to include/linux/fs.h
 static inline struct inode *file_inode(struct file *f)
 {
 	return f->f_path.dentry->d_inode;
-}
+}*/
 
 static inline bool is_dot_dotdot(const struct qstr *str)
 {
