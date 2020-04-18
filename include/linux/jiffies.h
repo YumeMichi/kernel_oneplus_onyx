09f7f6e9a7bbaf1c5bf54cb04c40cef25df500f0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-18 13:47:43 +0530

    kernel: Add 300 Hz time conversion constants
     * Obtained from mainline kernel/time/timeconst.bc script
    
    Change-Id: I1589af0e1445190e84bcbdd1e59a4081a3283cde

--- include/linux/jiffies.h
+++ include/linux/jiffies.h
@@ -8,6 +8,35 @@
 #include <linux/timex.h>
 #include <asm/param.h>			/* for HZ */
 
+/* Time conversion constants for 300 Hz */
+#if HZ == 300
+#define HZ_TO_MSEC_MUL32        U64_C(0xD5555556)
+#define HZ_TO_MSEC_ADJ32        U64_C(0x2AAAAAAA)
+#define HZ_TO_MSEC_SHR32        30
+#define MSEC_TO_HZ_MUL32        U64_C(0x9999999A)
+#define MSEC_TO_HZ_ADJ32        U64_C(0x1CCCCCCCC)
+#define MSEC_TO_HZ_SHR32        33
+#define HZ_TO_MSEC_NUM          10
+#define HZ_TO_MSEC_DEN          3
+#define MSEC_TO_HZ_NUM          3
+#define MSEC_TO_HZ_DEN          10
+
+#define HZ_TO_USEC_MUL32        U64_C(0xD0555556)
+#define HZ_TO_USEC_ADJ32        U64_C(0xAAAAA)
+#define HZ_TO_USEC_SHR32        20
+#define USEC_TO_HZ_MUL32        U64_C(0x9D495183)
+#define USEC_TO_HZ_ADJ32        U64_C(0x7FFCB923A29)
+#define USEC_TO_HZ_SHR32        43
+#define HZ_TO_USEC_NUM          10000
+#define HZ_TO_USEC_DEN          3
+#define USEC_TO_HZ_NUM          3
+#define USEC_TO_HZ_DEN          10000
+#define HZ_TO_NSEC_NUM          10000000
+#define HZ_TO_NSEC_DEN          3
+#define NSEC_TO_HZ_NUM          3
+#define NSEC_TO_HZ_DEN          10000000
+#endif
+
 /*
  * The following defines establish the engineering parameters of the PLL
  * model. The HZ variable establishes the timer interrupt frequency, 100 Hz
