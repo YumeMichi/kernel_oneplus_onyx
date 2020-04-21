901d6f50110e62f3f12661d63f5dc1c123e50f81
YumeMichi <do4suki@gmail.com>
2020-04-22 04:26:21 +0800

    Revert "msm: clock-krait-8974: Underclock CPU to 1958 MHz"
    This reverts commit 1957b3ab1daa10befe163f4edbb14380752411fa.
    
    Reason for revert: Unfortunately onyx is too old.
    
    Change-Id: I5661e46fcf0221d6b928329cc2069edaa420a53b
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -590,19 +590,6 @@ module_param_string(table_name, table_name, sizeof(table_name), S_IRUGO);
 static unsigned int pvs_config_ver;
 module_param(pvs_config_ver, uint, S_IRUGO);
 
-#ifdef CONFIG_MACH_MSM8974_15055
-static unsigned int no_cpu_underclock;
-
-static int __init get_cpu_underclock(char *cpu_uc)
-{
-	if (!strncmp(cpu_uc, "1", 1))
-		no_cpu_underclock = 1;
-
-	return 0;
-}
-__setup("no_underclock=", get_cpu_underclock);
-#endif
-
 static int clock_krait_8974_driver_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -717,16 +704,6 @@ static int clock_krait_8974_driver_probe(struct platform_device *pdev)
 			rows = ret;
 		}
 	}
-
-#ifdef CONFIG_MACH_MSM8974_15055
-	/* Underclock to 1958MHz for better UX */
-	if (!no_cpu_underclock) {
-		while (rows--) {
-			if (freq[rows - 1] == 1958400000)
-				break;
-		}
-	}
-#endif
 
 	krait_update_uv(uv, rows, pvs ? 25000 : 0);
 
