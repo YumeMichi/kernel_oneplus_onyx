258825403d95ed3509aaaf3c2e3fb4123914dc99
YumeMichi <do4suki@gmail.com>
2020-04-22 00:52:02 +0800

    Revert "ALSA: usb-audio: Fix regression by disconnection-race-fix patch"
    This reverts commit 115b96e58609cff057d22d0e6118dae093763461.
    
    Reason for revert: For incoming backport from Linux 3.10.
    
    Change-Id: I3b37b59448e9f13e05a126bb7ffbbe00a9c45bde
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- sound/usb/endpoint.c
+++ sound/usb/endpoint.c
@@ -148,8 +148,10 @@ void snd_usb_release_substream_urbs(struct snd_usb_substream *subs, int force)
 	int i;
 
 	/* stop urbs (to be sure) */
-	deactivate_urbs(subs, force, 1);
-	wait_clear_urbs(subs);
+	if (!subs->stream->chip->shutdown) {
+		deactivate_urbs(subs, force, 1);
+		wait_clear_urbs(subs);
+	}
 
 	for (i = 0; i < MAX_URBS; i++)
 		release_urb_ctx(&subs->dataurb[i]);
