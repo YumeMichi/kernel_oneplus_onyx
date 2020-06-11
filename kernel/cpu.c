3046088c08ad0ecc7d054f7480088bd3e8c0ff17
Kazuki Hashimoto <kaz205@tuta.io>
2020-06-12 01:09:37 +0900

    kernel: Silence unneeded logging that occurs upon device wakeup
    There is absolutely no reason to print these as they are merely indicators showing that the device had successfully woken up.
    
    Change-Id: Id884aa433e278e3323021a82e743e891fe9e5ab7
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -451,7 +451,6 @@ int disable_nonboot_cpus(void)
 	cpumask_clear(frozen_cpus);
 	arch_disable_nonboot_cpus_begin();
 
-	printk("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
 			continue;
@@ -496,17 +495,12 @@ void __ref enable_nonboot_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	printk(KERN_INFO "Enabling non-boot CPUs ...\n");
-
 	arch_enable_nonboot_cpus_begin();
 
 	for_each_cpu(cpu, frozen_cpus) {
 		error = _cpu_up(cpu, 1);
-		if (!error) {
-			printk(KERN_INFO "CPU%d is up\n", cpu);
-			continue;
-		}
-		printk(KERN_WARNING "Error taking CPU%d up: %d\n", cpu, error);
+		if (error)
+			printk(KERN_WARNING "Error taking CPU%d up: %d\n", cpu, error);
 	}
 
 	arch_enable_nonboot_cpus_end();
